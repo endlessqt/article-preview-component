3e3639a046313dd37d86bbd0f622e71cc238aced
endless_suffering <nocturne6667@gmail.com>
2020-08-21 00:56:06 +0300

    set up parcel and initial layout

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Manrope", "Helvetica", "Arial", "sans-serif"],
+      },
+      colors: {
+        dark: "var(--dark)",
+        lightdark: "var(--lightdark)",
+        lightblue: "var(--lightblue)",
+        lightdarkblue: "var(--lightdarkblue)",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
